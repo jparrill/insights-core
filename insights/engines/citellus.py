99a4eb7055cd4fd8aa6f3feafe52f1256602a70d
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2018-06-07 15:56:51 +0200

    Added function exec engine, to execute the citellus engine through insights-cli

--- insights/engines/citellus.py
+++ insights/engines/citellus.py
@@ -1,4 +1,5 @@
 import os.path
+from citellusclient import shell as citellus
 
 class CitellusEngine(object):
     """
@@ -10,4 +11,17 @@ class CitellusEngine(object):
         print "Loading Engine: {}".format(self.__class__.__name__)
         print os.path.abspath(sosreport)
 
+
+    def exec_engine(self, path=False, plugins=False, forcerun=False, include=None, exclude=None):
+        
+        results = citellus.docitellus(path=path, plugins=plugins, forcerun=forcerun, include=include, exclude=exclude, quiet=True)
+
+        # Process plugin output from multiple plugins to be returned as a dictionary of ID's for each plugin
+        new_dict = {}
+        for item in results:
+            name = results[item]['id']
+            new_dict[name] = dict(results[item])
+        del results
+
+        return new_dict
 
