7fe085f76811510ff0569d0bdd36bb52067def56
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2018-06-08 13:12:37 +0200

    Working with results

--- insights/engines/citellus.py
+++ insights/engines/citellus.py
@@ -12,13 +12,12 @@ class CitellusEngine(object):
         print os.path.abspath(sosreport)
 
 
-    def get_plugins(self):
-        # TODO: Still failling because I need to preload the plugins from Citellus
-        pass
-
+    def get_plugins(self, options, filter=False):
+        return citellus.findallplugins()
 
     def exec_engine(self, path=False, plugins=False, forcerun=False, include=None, exclude=None):
-        
+        plugins = self.get_plugins(include, filter=True)
+        #ids = citellus.getids(plugins=plugins, include=include, exclude=exclude)
         results = citellus.docitellus(path=path, plugins=plugins, forcerun=forcerun, include=include, exclude=exclude, quiet=True)
 
         # Process plugin output from multiple plugins to be returned as a dictionary of ID's for each plugin
